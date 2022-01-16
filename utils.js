a11d432b96b9d9334fe284cd10a1ebbd50e1ff9b
BrynGhiffar <bryn.ghiffar@gmail.com>
2022-01-17 00:01:19 +0700

    Added comments to js utilities

--- utils.js
+++ utils.js
@@ -1,23 +1,29 @@
+
+// yields values in the range [start, end)
 function* range(start, end) {
     for (let i = start; i < end; i++) {
         yield i;
     }
 }
 
+// returns a random integer in the range [start, end]
 function randint(start, end) {
     return Math.floor(Math.random() * (end - start + 1)) + start;
 }
 
-function* randarray_(start, length) {
+// returns an arary of random integers
+function* randarray_(start, jump, length) {
     let current = start;
     let add;
     for (let i = 0; i < length; i++) {
         yield current;
-        add = (Math.random() - 1 / 2) * 20;
+        add = (Math.random() - 1 / 2) * jump;
         current += add * Math.pow(-1, ((current + add) <= 0));
     }
 }
 
+// returns true if a year is a leap year. Used to get number of days
+// in february
 function isLeapYear(year) {
     if (year % 400 == 0) {
         return true;
@@ -34,6 +40,8 @@ function isLeapYear(year) {
     return false;
 }
 
+// returns the number of days in a given month in a given year
+// For example if isLeapYear(year) then february has 29 days.
 function getDays(month, year) {
     // returns the number of days in a particular month of a particular year
     let months = {
